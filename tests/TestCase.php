caae3469b0c92d085b24503c639e88912ae63615
Tristan <tristanbudd@outlook.com>
2026-03-23 17:32:48 +0000

    Fix test case failing

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,12 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+        
+        // Tells Laravel to ignore Vite assets during testing
+        // so CI doesn't crash looking for a missing manifest.json
+        $this->withoutVite();
+    }
 }
