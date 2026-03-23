b5439ef45ecfb25f4a9ba2b83cf9d5e633df0d9e
Tristan <tristanbudd@outlook.com>
2026-03-23 17:47:45 +0000

    Fix pint path

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,7 +9,7 @@ abstract class TestCase extends BaseTestCase
     protected function setUp(): void
     {
         parent::setUp();
-        
+
         // Tells Laravel to ignore Vite assets during testing
         // so CI doesn't crash looking for a missing manifest.json
         $this->withoutVite();
