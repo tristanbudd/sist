bc8c51b9fa25e03b9c0a078632edcf031c29d0fc
Tristan <tristanbudd@outlook.com>
2026-04-09 13:51:38 +0100

    Build documentation on deploy

--- docker/entrypoint/start.sh
+++ docker/entrypoint/start.sh
@@ -16,4 +16,13 @@ chown -R www-data:www-data storage bootstrap/cache
 find storage bootstrap/cache -type d -exec chmod 775 {} \;
 find storage bootstrap/cache -type f -exec chmod 664 {} \;
 
+if [ "${RUN_DEPLOY_TASKS_ON_STARTUP:-true}" = "true" ]; then
+	echo "Running deploy tasks (migrate/cache/docs)..."
+	php artisan migrate --force
+	php artisan config:cache
+	php artisan route:cache
+	php artisan view:cache
+	php artisan scribe:generate
+fi
+
 exec "$@"
