e2721b5fec8623aef37c24304d17a9f1d26309b5
Tristan <contact@tristanbudd.com>
2026-04-22 10:05:11 +0100

    Fix StatusBar Routing

--- resources/js/Layouts/MainLayout.tsx
+++ resources/js/Layouts/MainLayout.tsx
@@ -1,5 +1,5 @@
 import { PropsWithChildren } from 'react';
-import StatusBar from '../../Components/StatusBar';
+import StatusBar from '../Components/StatusBar';
 
 export default function MainLayout({ children }: PropsWithChildren) {
     return (
